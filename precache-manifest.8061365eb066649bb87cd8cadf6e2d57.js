56260da9362165b03ad4b1e7db3e1888e6220ab1
Colin Brayton <gringolalia@gmail.com>
2020-07-24 10:49:16 -0300

    deploy

---
+++ precache-manifest.8061365eb066649bb87cd8cadf6e2d57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd43fdaef36225b4ff9",
+    "url": "/missile-crisis-three/css/about.08318103.css"
+  },
+  {
+    "revision": "251fc28f9ee226534b33",
+    "url": "/missile-crisis-three/css/app.f3c2a390.css"
+  },
+  {
+    "revision": "2fa1c071c980dec4b333",
+    "url": "/missile-crisis-three/css/chunk-vendors.755a9159.css"
+  },
+  {
+    "revision": "58fd172793a8bbb613317c971d037804",
+    "url": "/missile-crisis-three/img/breugeltorre.58fd1727.jpg"
+  },
+  {
+    "revision": "bb5c37c857952e00635c8f1a62421975",
+    "url": "/missile-crisis-three/index.html"
+  },
+  {
+    "revision": "7dd43fdaef36225b4ff9",
+    "url": "/missile-crisis-three/js/about.ec9e1649.js"
+  },
+  {
+    "revision": "251fc28f9ee226534b33",
+    "url": "/missile-crisis-three/js/app.c7fb8eec.js"
+  },
+  {
+    "revision": "2fa1c071c980dec4b333",
+    "url": "/missile-crisis-three/js/chunk-vendors.27c486b8.js"
+  },
+  {
+    "revision": "048d20e93e4e560dbba898e3e459423f",
+    "url": "/missile-crisis-three/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/missile-crisis-three/robots.txt"
+  }
+]);
